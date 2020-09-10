8408c7aae3c4277e818f8d44a97e42c947c762ad
Nico Epp <nicoeppfriesen@gmail.com>
2020-09-10 11:58:48 -0700

    Subscription columns in admin list display

--- backend/streetsignup/admin.py
+++ backend/streetsignup/admin.py
@@ -5,3 +5,4 @@ from .models import Subscription
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
     search_fields = ['street__name', 'name', 'church']
+    list_display = ['name', 'church', 'street', 'created_at', 'verified']
