54f439d5e4e7b02af93959d6dd62a1ce7b782b88
Nico Epp <nicoeppfriesen@gmail.com>
2020-09-20 15:17:50 -0700

    Show the new 'verified' flag in admin list view

--- backend/streetsignup/admin.py
+++ backend/streetsignup/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from .models import Subscription
 
 
+def contact_verified(obj):
+    return obj.contact.verified
+
+
+contact_verified.short_description = 'Verified'
+contact_verified.boolean = True
+
+
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
+    # exclude_fields and readonly_fields
     search_fields = ['street__name', 'name', 'church']
-    list_display = ['name', 'church', 'street', 'created_at', 'verified']
+    list_display = ['name', 'church', 'street', 'created_at', contact_verified]
