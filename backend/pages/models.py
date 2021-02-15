5a3a08778cd06a68b3b0e8193ec86879684f0a78
Nico Epp <nicoeppfriesen@gmail.com>
2021-02-15 15:57:24 -0800

    Images having their own Streamfield block type

--- backend/pages/models.py
+++ backend/pages/models.py
@@ -4,13 +4,16 @@ from wagtail.core.models import Page
 from wagtail.core.fields import StreamField
 from wagtail.core import blocks
 from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.images.blocks import ImageChooserBlock
+
+RICHTEXT_FEATURES = ['h2', 'h3', 'h4', 'ul', 'ol', 'bold', 'italic', 'link', 'hr']
 
 
 class HomePage(Page):
     body = StreamField([
         ('title', blocks.CharBlock(form_classname='title', required=False)),
         ('paragraph', blocks.TextBlock(form_classname='full')),
-        ('rich', blocks.RichTextBlock(form_classname='full')),
+        ('rich', blocks.RichTextBlock(form_classname='full', features=RICHTEXT_FEATURES)),
     ])
 
     content_panels = Page.content_panels + [
@@ -22,9 +25,15 @@ class HomePage(Page):
 
 class SubPage(Page):
     body = StreamField([
-        ('title', blocks.CharBlock(form_classname='title')),
+        ('title', blocks.CharBlock(form_classname='title', icon='title')),
         ('paragraph', blocks.TextBlock(form_classname='full')),
-        ('rich', blocks.RichTextBlock(form_classname='full')),
+        ('rich', blocks.RichTextBlock(form_classname='full', features=RICHTEXT_FEATURES)),
+        ('images', blocks.StreamBlock(
+            [
+                ('image', ImageChooserBlock()),
+            ],
+            icon='image'
+        )),
     ])
 
     content_panels = Page.content_panels + [
