49a1b978c56a9872a71785811088fe9b10285103
Nico Epp <nicoeppfriesen@gmail.com>
2021-04-18 15:38:27 -0700

    Store uploaded images on Google Cloud Storage
    in production since Heroku's filesystem is "temporal"
    
    https://docs.wagtail.io/en/stable/advanced_topics/deploying.html#cloud-storage

---
+++ backend/settings/gcloud_credentials.py
@@ -0,0 +1,26 @@
+import json
+import os
+from json import JSONDecodeError
+
+from google.oauth2 import service_account
+from google.auth import crypt
+
+
+def google_credentials_from_env(env_key):
+    try:
+        credentials_json = os.environ.get(env_key)
+        info = json.loads(credentials_json or '')
+    except (TypeError, JSONDecodeError) as e:
+        raise ValueError('Service account info was not in the expected format, should be valid json')
+
+    if 'token_uri' not in info and 'client_email' not in info:
+        raise ValueError('Service account info not in the expected format, both token_uri and client_email required')
+
+    signer = crypt.RSASigner.from_service_account_info(info)
+
+    return service_account.Credentials(
+        signer,
+        service_account_email=info["client_email"],
+        token_uri=info["token_uri"],
+        project_id=info.get("project_id")
+    )
--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -1,4 +1,5 @@
 import django_heroku
+from .gcloud_credentials import google_credentials_from_env
 
 from .dev import *
 
@@ -10,3 +11,7 @@ DEBUG = False
 
 # Overwrite what django_heroku tried to overwrite
 STATIC_ROOT = BASE_DIR / 'dist' / 'static'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+GS_BUCKET_NAME = 'prayer-walk-heroku'
+GS_CREDENTIALS = google_credentials_from_env('GOOGLE_CREDENTIALS')
