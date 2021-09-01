c7e6d530a65403690627721db363d84c76286172
Nico Epp <nicoeppfriesen@gmail.com>
2021-08-31 21:38:50 -0700

    Enable logs coming from Django itself
    including Wagtail

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -116,6 +116,20 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
