e239ec101563d7e6347c98df6fc98098aa571413
Nico Epp <nicoeppfriesen@gmail.com>
2022-01-01 17:29:29 -0800

    Get sk from environment variable in production

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -18,3 +18,10 @@ STATIC_ROOT = BASE_DIR / 'dist' / 'static'
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 GS_BUCKET_NAME = os.environ.get('GOOGLE_STORAGE_BUCKET')
 GS_CREDENTIALS = get_google_credentials(os.environ.get('GOOGLE_CREDENTIALS'))
+
+sk = os.environ.get('SECRET_KEY', None)
+
+if not sk:
+    raise Exception('Please provide a SECRET_KEY env var! You can use Django\'s '
+                    'get_random_secret_key() function to generate one.')
+SECRET_KEY = sk
