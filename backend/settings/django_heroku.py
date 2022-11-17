4d4f37b7947974805624f3ebc91816c891724aa8
Nico Epp <nicoeppfriesen@gmail.com>
2022-11-16 20:52:28 -0800

    Adjust config for Fly

--- backend/settings/django_heroku.py
+++ backend/settings/django_heroku.py
@@ -30,7 +30,7 @@ def settings(config, *, db_colors=False, databases=True, test_runner=True, stati
             logger.info('Adding $DATABASE_URL to default DATABASE Django setting.')
 
             # Configure Django for DATABASE_URL environment variable.
-            config['DATABASES']['default'] = dj_database_url.config(conn_max_age=MAX_CONN_AGE, ssl_require=True)
+            config['DATABASES']['default'] = dj_database_url.config(conn_max_age=MAX_CONN_AGE, ssl_require=False)
 
             logger.info('Adding $DATABASE_URL to TEST default DATABASE Django setting.')
 
